eaac1d2aa334d31bc118c725593f35edf83abb75
Minneh Mugo <minneh.mugo@gmail.com>
2017-05-12 14:26:00 +0300

    add class declaratoins to EndangeredAnimalTest, SightingTest and AnimalTest; add testAnimal and setup method to AnimalTest

---
+++ src/main/java/Animal.java
@@ -0,0 +1,12 @@
+import org.sql2o.*;
+import java.util.List;
+import java.util.ArrayList;
+import java.sql.Timestamp;
+
+public class Animal{
+  private int id;
+  private String name;
+
+  
+
+}
--- src/test/java/AnimalTest.java
+++ src/test/java/AnimalTest.java
@@ -1,3 +1,16 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.sql2o.*;
+import java.util.Arrays;
+import java.util.List;
+
 public class AnimalTest{
-  
+
+  Private Animal testAnimal;
+
+  @Before
+  public void setup(){
+    Animal testAnimal = new Animal("Orangutan");
+  }
+
 }
