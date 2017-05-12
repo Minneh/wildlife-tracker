545332258bc6789b5f1a3c4c3703c4ce58b2c286
Minneh Mugo <minneh.mugo@gmail.com>
2017-05-12 15:04:19 +0300

    add @Before to Sighting test

--- src/test/java/SightingTest.java
+++ src/test/java/SightingTest.java
@@ -6,5 +6,13 @@ public class SightingTest{
   @Rule
   public DatabaseRule database = new DatabaseRule();
 
+  private Sighting testSighting;
+
+  @Before
+  public void setUp() {
+    Timestamp now = new Timestamp(new Date().getTime());
+    testSighting = new Sighting(1, "By the lower pond", "Ole Nkrumah", now);
+  }
+
 
 }
