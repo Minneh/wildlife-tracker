e71d9a07210bb7c94188a554de53e0d47ca1af8d
Minneh Mugo <minneh.mugo@gmail.com>
2017-05-14 22:40:10 +0300

    add getLocation function to Sighting.java

--- src/main/java/Sighting.java
+++ src/main/java/Sighting.java
@@ -7,14 +7,14 @@ public class Sighting implements DatabaseManagement{
   private int id;
   private int animal_id;
   private String location;
-  private String ranger;
+  private String ranger_name;
   private Timestamp time_seen;
 
-  public Sighting(int animal_id, String location, String ranger) {
+  public Sighting(int animal_id, String location, String ranger_name, Timestamp time_seen) {
       this.animal_id = animal_id;
       this.location = location;
-      this.ranger = ranger;
-      this.time_seen = new Timestamp(new Date().getTime());
+      this.ranger_name = ranger_name;
+      this.time_seen = time_seen;
   }
 
   public int getId(){
@@ -22,22 +22,22 @@ public class Sighting implements DatabaseManagement{
   }
 
   public int getAnimalId(){
-    return id;
+    return animal_id;
   }
 
-  public int getId(){
-    return id;
+  public String getLocation(){
+    return location;
   }
 
   @Override
   public void save() {
-    String sql = "INSERT INTO sightings (animal_id, location, ranger, time_seen) VALUES (:animal_id, :location, :ranger, :time_seen);";
+    String sql = "INSERT INTO sightings (animal_id, location, ranger_name, time_seen) VALUES (:animal_id, :location, :ranger_name, :time_seen);";
 
     try (Connection con = DB.sql2o.open()) {
         this.id = (int) con.createQuery(sql, true)
             .addParameter("animal_id", this.animal_id)
             .addParameter("location", this.location)
-            .addParameter("ranger", this.ranger)
+            .addParameter("ranger_name", this.ranger_name)
             .addParameter("time_seen",this.time_seen)
             .executeUpdate()
             .getKey();
