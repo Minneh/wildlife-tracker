3877e6c343330df43c0c37ed1b404776ae93dc6b
Minneh Mugo <minneh.mugo@gmail.com>
2017-06-29 19:52:32 +0300

    add homepage images and default route

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -19,5 +19,12 @@ public class App {
     port(getHerokuAssignedPort());
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("animals", Animal.all());
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
 }
