03378b9518eed200516f0ca424ddddd675c6c0ad
Minneh Mugo <minneh.mugo@gmail.com>
2017-06-30 19:43:57 +0300

    add new animal route

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -26,5 +26,11 @@ public class App {
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
+
+    get("/animals/new", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/animal-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
 }
