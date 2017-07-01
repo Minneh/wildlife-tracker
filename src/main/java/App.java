1a09bc0ab4496e78cbca73e6eb3cadbeb057653a
Minneh Mugo <minneh.mugo@gmail.com>
2017-07-01 10:47:04 +0300

    add animal list layout route

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -27,6 +27,13 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    get("/animals", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("animals", Animal.all());
+      model.put("template", "templates/animals.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
     get("/animals/new", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
       model.put("template", "templates/animal-form.vtl");
